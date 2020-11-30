12282dfd0477825eab2abb7d2a1479905b71d285
Dan Prince <djprince@ucdavis.edu>
2020-11-30 08:49:06 -0800

    added download files

--- 0000_Download.sh
+++ 0000_Download.sh
@@ -4,12 +4,16 @@ mkdir slurm_outs/
 mkdir 1001_sequence/
 mkdir 1001_sequence/plates/
 cd 1001_sequence/plates/
-wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/
-wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/
-wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/
-wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/
-wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/
-wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/
+wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/CAACATAT_R1.fastq.gz
+wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/CAACATAT_R2.fastq.gz
+wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/GTGCATAT_R1.fastq.gz
+wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/GTGCATAT_R2.fastq.gz
+wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/TTTGATAT_R1.fastq.gz
+wget http://agri.cse.ucdavis.edu/~djprince/LSCHUB/seq/TTTGATAT_R2.fastq.gz
 
-
-
+gunzip CAACATAT_R1.fastq.gz
+gunzip CAACATAT_R2.fastq.gz
+gunzip GTGCATAT_R1.fastq.gz
+gunzip GTGCATAT_R2.fastq.gz
+gunzip TTTGATAT_R1.fastq.gz
+gunzip TTTGATAT_R2.fastq.gz
